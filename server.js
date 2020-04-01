2bb60aa66af9fad2378b04a9a0083416fdf66e77
cshekar86 <gubbi.c@husky.neu.edu>
2020-04-01 14:42:04 -0400

    changes to node

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(function (req, res, next) {
 require('./controllers/quiz.controller.server')(app)
 require('./controllers/question.controller.server')(app)
 
-app.listen(3000)
-
-
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
